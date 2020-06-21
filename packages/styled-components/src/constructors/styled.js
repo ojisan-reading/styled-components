47f6cbd473cf770e928c1f0b1fb379725360735b
sadness_ojisan <sadness.ojisan@gmail.com>
2020-06-22 05:41:25 +0900

    reading

--- packages/styled-components/src/constructors/styled.js
+++ packages/styled-components/src/constructors/styled.js
@@ -5,6 +5,10 @@ import domElements from '../utils/domElements';
 
 import type { Target } from '../types';
 
+/**
+ * styled は constructWithOptions(StyledComponent, tag)
+ * @param {*} tag
+ */
 const styled = (tag: Target) =>
   /** @疑問: rulesいらないの？nullableじゃなくね？ */
   constructWithOptions(StyledComponent, tag);
@@ -13,6 +17,8 @@ const styled = (tag: Target) =>
 
 /** styled.div などの実装を登録していってる。
  * これはdefault import時に初回実行されるもの、ここで作られたものが登録されていく
+ *
+ * styledの実行前に呼ばれるので、登録できる。
  */
 domElements.forEach(domElement => {
   styled[domElement] = styled(domElement);
